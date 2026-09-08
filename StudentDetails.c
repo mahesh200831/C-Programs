f61707450e38e2b0d19549d98b122f5946ea1bed
Mahesh Chate <maheshchate2008@gmail.com>
2026-09-08 21:23:38 +0530

    Add files via upload

---
+++ StudentDetails.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ printf("Student Details\n Name: Mahesh Chate\n Roll No: 17\n Division: SOC-6\n College Name: MIT-ADT\n Address: Pune\n");
+  
+ return 0;
+}
