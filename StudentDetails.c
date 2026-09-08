52f46fc7f1d23835fe7dbaa9e8fe6519b87607ef
Mahesh Chate <maheshchate2008@gmail.com>
2026-09-08 21:33:12 +0530

    Delete StudentDetails.c

--- StudentDetails.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<conio.h>
-int main()
-{
- printf("Student Details\n Name: Mahesh Chate\n Roll No: 17\n Division: SOC-6\n College Name: MIT-ADT\n Address: Pune\n");
-  
- return 0;
-}
