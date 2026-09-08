f66377b2615bcf3ec9f40aeed38242476564d845
Mahesh Chate <maheshchate2008@gmail.com>
2026-09-08 21:59:51 +0530

    Add files via upload

---
+++ Height.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int age;
+float height;
+printf("Enter Your Age:");
+scanf("%d",&age);
+printf("Enter Your Height:");
+scanf("%f",&height);
+printf("Your Age is: %d\n", age);
+printf("Your Height is: %f\n", height);
+return 0;
+}
