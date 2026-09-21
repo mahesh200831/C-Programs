e034c7cbda2514f3d126106b77b5fb2ab1428b48
Mahesh Chate <maheshchate2008@gmail.com>
2026-09-21 21:54:30 +0530

    Add files via upload

---
+++ Unit 1/Addition.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+ int num1,num2,num3,sum;
+ printf("Enter First Number: ");
+ scanf("%d",&num1);
+ printf("Enter Second Number: ");
+ scanf("%d",&num2);
+ printf("Enter Third Number: ");
+ scanf("%d",&num3);
+ sum = num1 + num2 + num3;
+ printf("Sum = %d",sum);
+ return 0;
+}
---
+++ Unit 1/StudentDetails.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+printf("Student Details\n");
+printf("Name: Mahesh Chate\n");
+printf("Roll No: 17\n");
+printf("Division: SOC-6\n");
+printf("College Name: MIT-ADT\n");
+printf("Address: Pune\n");
+
+ return 0;
+}
